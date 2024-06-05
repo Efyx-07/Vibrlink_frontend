8a8f8f01c1c3e0a48d2a6b5cbdc2785558dfe88d
François-Xavier Savary <fx.savary@orange.fr>
2024-06-05 12:06:49 +0200

    setup HomePage

---
+++ src/app/components/Home-Page/Hero.tsx
@@ -0,0 +1,43 @@
+"use client"
+
+import { useEffect } from 'react';
+import { useUserStore } from '@/stores';
+import { useRouter } from 'next/navigation';
+//import FormButton from '../common/FormButton';
+import './Hero.scss';
+
+export default function Hero() {
+
+    // get the login status of the user to display the wanted buttons
+    const isLogged: boolean = useUserStore(state => state.isLoggedIn);
+
+    const router = useRouter();
+
+    // watch and update the login status of the user
+    useEffect(() => {
+    }, [isLogged]);
+
+    return (
+        <div className="hero-container">
+            <div className="text-container">
+                <h1>Hey, it&apos;s the <span>big day </span>!<br/>Ready to launch your new music ?</h1>
+                <p>As a musician, we know how special this day is and how exciting it is to share your new release with your fan community. Let&apos;s do it!</p>
+            </div>
+            {/* <div className="buttons-container">
+                {isLogged ? 
+                    (
+                        <FormButton type="button" name="Create a new vibrLink" onClick={() => navigate('/new-vibrlink')} id="hero-button"/>
+                    )
+                    :
+                    (
+                        <>
+                            <FormButton id="hero-button" type="button" name="Create a free account" onClick={() => navigate('/signup')}/>
+                            <p>or</p>
+                            <FormButton id="hero-button" type="button" name="Sign in" onClick={() => navigate('/login')}/>
+                        </>
+                    )
+                }
+            </div> */}
+        </div>
+    )
+}

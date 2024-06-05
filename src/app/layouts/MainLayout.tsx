f8bd55e96207eb61931d3030e49bce7f68d44160
François-Xavier Savary <fx.savary@orange.fr>
2024-06-05 16:01:29 +0200

    update

--- src/app/layouts/MainLayout.tsx
+++ src/app/layouts/MainLayout.tsx
@@ -4,6 +4,7 @@ import useAppInitializer from "@/hooks/AppInitializer";
 import LoadingSpinner from "../components/Shared/LoadingSpinner";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import './MainLayout.scss';
 
 export default function MainLayout({
   children,
@@ -13,15 +14,21 @@ export default function MainLayout({
 
   const loading = useAppInitializer();
 
-  if (loading) {
-    return <div><LoadingSpinner /></div>;
-  }
-
   return (
     <>
-      <Header/>
-      <main>{children}</main>
-      <Footer/>
+      {loading ? 
+        (
+          <div className="spinner-container"><LoadingSpinner /></div>
+        )
+        :
+        (
+          <>
+            <Header/>
+            <main>{children}</main>
+            <Footer/>
+          </>
+        )
+      }
     </>
   );
 }

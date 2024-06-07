814a60a772e0ab8614eba8690ed8b8b38598cd1e
Efyx-07 <fx.savary@orange.fr>
2024-06-08 01:02:49 +0200

    sigup page

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,34 @@
+"use client"
+
+import MainLayout from "../layouts/MainLayout";
+import FormPageMessage from "../components/Shared/FormPageMessage";
+import StyledSeparator from "../components/Shared/StyledSeparator";
+import SignupForm from "../components/User-forms/SignupForm";
+import '../../assets/sass/pages-common-styles.scss';
+import '../../assets/sass/messageAndFormContainer.scss';
+import '../../assets/sass/form-container.scss';
+
+export default function SignupPage() {
+
+    const mainTextPrimary: string = 'Create';
+    const mainTextSecondary: string = ' a free account';
+    const subText: string = 'Let\'s get started and enjoy using our service by creating your account. It\'s free !';
+
+    return (
+        <>
+            <MainLayout>
+                <div className='page'>
+                    <div className="content">
+                        <div className="message-and-form-container">
+                            <FormPageMessage mainTextPrimary={mainTextPrimary} mainTextSecondary={mainTextSecondary} subText={subText} />
+                            <StyledSeparator icon="simple-icons:freepik" />
+                            <div className="form-container">
+                                <SignupForm />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </MainLayout>
+        </>
+    )
+};
